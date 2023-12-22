714d360a79a7d87122cb54651ed16d0951db7382
Alexa Vizcaino <146246560+alexavs1991@users.noreply.github.com>
2023-12-22 00:55:28 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const endpoint = "https://playground.4geeks.com/apis/fake/todos/user/alexavs1991";
 
-//create your first component
+
+
 const Home = () => {
+
+	const [taskInput, setTaskInput] = useState("");
+	const [myTodos, setMyTodos] = useState([]);
+
+	const getTasks = async () => {
+		try {
+			const response = await fetch(endpoint);
+			const data = await response.json();
+			setMyTodos(data);
+		} catch (error) {
+			console.log(error, "ERROR the task is wrong")
+		}
+	};
+
+	const editTask = async (task) => {
+		const response = await fetch(endpoint, {
+			method: 'PUT',
+			body: JSON.stringify(task),
+			headers: { 'Content-Type': 'application/json' }
+		})
+	}
+
+
+	const handleClick = () => {
+		const newTask = [...myTodos, { done: false, label: taskInput }]
+		setMyTodos(newTask)
+		setTaskInput("")
+		editTask(newTask)
+	}
+
+
+	const handleChange = (e) => {
+		setTaskInput(e.target.value);
+	}
+
+	const deleteTask = async (index) => {
+		const eliminateTask = myTodos.filter((item, indice) => indice !== index);
+		setMyTodos(eliminateTask)
+		editTask(eliminateTask)
+
+	}
+
+
+	useEffect(() => {
+		getTasks();
+	}, [])
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container">
+				<div className="row mx-1">
+					<div>
+						<h1>
+							Todo List
+						</h1>
+
+						<div className="d-flex justify-content-between">
+							<input className="input form-control" onChange={handleChange}></input>
+							<button onClick={handleClick} className="button btn btn-success" type="submit">
+								Add
+							</button>
+						</div>
+					</div>
+					<div className="my-3">
+						<ul>
+							{myTodos.map((task, index) => {
+								return (
+									<div className="my-3 d-flex justify-content-between" key={index}>
+										{task.label}
+										<button className="trash btn btn-danger" onClick={() => deleteTask(index)}>
+											<i class="fas fa-trash-alt"></i>
+										</button>
+									</div>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+			</div>
+		</>
 	);
-};
+}
+
 
 export default Home;
